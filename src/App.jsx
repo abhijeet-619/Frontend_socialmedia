cbcd1b903d4ceb606a357230c0f03f5b672c3b1d
Abhijeet <abhijeetthamake07973@gmail.com>
2024-01-15 16:20:16 +0530

    frontend complete with home,login,profile,register pages

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import { Routes,Route,Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+
+function App() {
+  const { user } = useContext(AuthContext);
+  return (
+  <Routes>
+  <Route exact path='/' element={ user ? <Home/> :<Register/>  }/>
+  <Route path='/login' element={ user ? <Navigate replace to={"/"}/> :<Login/> }/>
+  <Route path='/register'element={ user ? <Navigate replace to={"/"}/> :<Register/> }/>
+  <Route path='/profile/:username' element={<Profile/>}/>
+ </Routes>
+ );
+}
+
+export default App;
+     
